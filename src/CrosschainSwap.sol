749212dbaffac8cbc50fc08c5992bdaa03287ca1
Soheil Parsaei <106927616+Soheilprs@users.noreply.github.com>
2023-10-05 14:45:24 +0330

    Update CrosschainSwap.sol

--- src/CrosschainSwap.sol
+++ src/CrosschainSwap.sol
@@ -1,83 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
-import "@zetachain/protocol-contracts/contracts/evm/Zeta.eth.sol";
-import "@zetachain/protocol-contracts/contracts/evm/tools/ZetaInteractor.sol";
-import "@zetachain/protocol-contracts/contracts/evm/interfaces/ZetaInterfaces.sol";
 import "@zetachain/toolkit/contracts/SwapHelperLib.sol";
 import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
-
-interface MultiChainValueErrors {
-    error ErrorTransferringZeta();
-
-    error ChainIdAlreadyEnabled();
-
-    error ChainIdNotAvailable();
-
-    error InvalidZetaValueAndGas();
-}
-
-contract MultiChainValue is ZetaInteractor, MultiChainValueErrors {
-    address public zetaToken;
-    // @dev map of valid chains to send Zeta
-    mapping(uint256 => bool) public availableChainIds;
-
-    // @dev Constructor calls ZetaInteractor's constructor to setup Connector address and current chain
-    constructor(address connectorAddress_, address zetaToken_) ZetaInteractor(connectorAddress_) {
-        if (zetaToken_ == address(0)) revert ZetaCommonErrors.InvalidAddress();
-        zetaToken = zetaToken_;
-    }
-
-    /**
-     * @dev Whitelist a chain to send Zeta
-     */
-    function addAvailableChainId(uint256 destinationChainId) external onlyOwner {
-        if (availableChainIds[destinationChainId]) {
-            revert ChainIdAlreadyEnabled();
-        }
-
-        availableChainIds[destinationChainId] = true;
-    }
-
-    /**
-     * @dev Blacklist a chain to send Zeta
-     */
-    function removeAvailableChainId(uint256 destinationChainId) external onlyOwner {
-        if (!availableChainIds[destinationChainId]) {
-            revert ChainIdNotAvailable();
-        }
-
-        delete availableChainIds[destinationChainId];
-    }
-
-    /**
-     * @dev If the destination chain is a valid chain, send the Zeta tokens to that chain
-     */
-    function send(uint256 destinationChainId, bytes calldata destinationAddress, uint256 zetaValueAndGas) external {
-        if (!availableChainIds[destinationChainId]) {
-            revert InvalidDestinationChainId();
-        }
-        if (zetaValueAndGas == 0) revert InvalidZetaValueAndGas();
-
-        bool success1 = ZetaEth(zetaToken).approve(address(connector), zetaValueAndGas);
-        bool success2 = ZetaEth(zetaToken).transferFrom(msg.sender, address(this), zetaValueAndGas);
-        if (!(success1 && success2)) revert ErrorTransferringZeta();
-
-        connector.send(
-            ZetaInterfaces.SendInput({
-                destinationChainId: destinationChainId,
-                destinationAddress: destinationAddress,
-                destinationGasLimit: 300000,
-                message: abi.encode(),
-                zetaValueAndGas: zetaValueAndGas,
-                zetaParams: abi.encode("")
-            })
-        );
-    }
-}
 
 contract Swap is zContract {
     error SenderNotSystemContract();
@@ -85,19 +12,12 @@ contract Swap is zContract {
     error NotEnoughToPayGasFee();
 
     SystemContract public immutable systemContract;
+    address public targetZRC20;
     uint256 constant BITCOIN = 18332;
+    uint256 constant ZETACHAIN = 7001;
 
     constructor(address systemContractAddress) {
         systemContract = SystemContract(systemContractAddress);
-    }
-
-    function bytesToBech32Bytes(bytes calldata data, uint256 offset) internal pure returns (bytes memory) {
-        bytes memory bech32Bytes = new bytes(42);
-        for (uint256 i = 0; i < 42; i++) {
-            bech32Bytes[i] = data[i + offset];
-        }
-
-        return bech32Bytes;
     }
 
     function onCrossChainCall(zContext calldata context, address zrc20, uint256 amount, bytes calldata message)
@@ -115,7 +35,7 @@ contract Swap is zContract {
 
         if (context.chainID == BITCOIN) {
             targetChainID = BytesHelperLib.bytesToUint32(message, 0);
-            recipient = bytesToBech32Bytes(message, 4);
+            recipient = BytesHelperLib.bytesToBech32Bytes(message, 4);
         } else {
             (uint32 targetChainID_, bytes32 recipient_, uint256 minAmountOut_) =
                 abi.decode(message, (uint32, bytes32, uint256));
@@ -124,7 +44,11 @@ contract Swap is zContract {
             minAmountOut = minAmountOut_;
         }
 
-        address targetZRC20 = systemContract.gasCoinZRC20ByChainId(targetChainID);
+        if (targetChainID == ZETACHAIN) {
+            targetZRC20 = 0x5F0b1a82749cb4E2278EC87F8BF6B618dC71a8bf;
+        } else {
+            targetZRC20 = systemContract.gasCoinZRC20ByChainId(targetChainID);
+        }
 
         uint256 outputAmount = SwapHelperLib._doSwap(
             systemContract.wZetaContractAddress(),
@@ -136,12 +60,42 @@ contract Swap is zContract {
             minAmountOut
         );
 
-        (address gasZRC20, uint256 gasFee) = IZRC20(targetZRC20).withdrawGasFee();
+        if (targetChainID == ZETACHAIN) {
+            address recipientAddr = BytesHelperLib.bytesToAddress(context.origin, 0);
+            (, uint256 gasFee) = IZRC20(zrc20).withdrawGasFee();
+            IWETH9(targetZRC20).approve(targetZRC20, gasFee);
+            IWETH9(targetZRC20).transfer(recipientAddr, outputAmount - gasFee);
+        } else {
+            (address gasZRC20, uint256 gasFee) = IZRC20(targetZRC20).withdrawGasFee();
 
-        if (gasZRC20 != targetZRC20) revert WrongGasContract();
-        if (gasFee >= outputAmount) revert NotEnoughToPayGasFee();
+            if (gasZRC20 != targetZRC20) revert WrongGasContract();
+            if (gasFee >= outputAmount) revert NotEnoughToPayGasFee();
 
-        IZRC20(targetZRC20).approve(targetZRC20, gasFee);
-        IZRC20(targetZRC20).withdraw(recipient, outputAmount - gasFee);
+            IZRC20(targetZRC20).approve(targetZRC20, gasFee);
+            IZRC20(targetZRC20).withdraw(recipient, outputAmount - gasFee);
+        }
     }
+}
+
+interface IWETH9 {
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Deposit(address indexed dst, uint256 wad);
+    event Withdrawal(address indexed src, uint256 wad);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 wad) external returns (bool);
+
+    function transfer(address to, uint256 wad) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 wad) external returns (bool);
+
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
 }
